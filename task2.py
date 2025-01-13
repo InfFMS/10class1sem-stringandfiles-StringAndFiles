876c3de5961780281af485bdf5b76aa982b9b299
IcpJ42 <iblion08@gmail.com>
2025-01-13 14:02:04 +0700

    Сделал 2 задание

--- task2.py
+++ task2.py
@@ -3,3 +3,9 @@
 # Найдите и замените все вхождения слова "Python" на слово "Питон" (регистр учитывать).
 # Запишите обновленный текст в новый файл с другим именем.
 # Выведите на экран сообщение о количестве произведённых замен.
+with open('task2.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+    replaced_text = text.replace('Python', 'Питон')
+    with open('task2_updated.txt', 'w', encoding='utf-8') as f_updated:
+        f_updated.write(replaced_text)
+        print(f'Заменено {text.count("Python")} слова на "Питон".')
